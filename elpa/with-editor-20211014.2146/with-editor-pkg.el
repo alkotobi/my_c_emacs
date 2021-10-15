fce4fdd1b9a5683b7933ae43693636ced38c6e63
merhab <merhab@gmail.com>
2021-10-15 01:50:49 +0100

    magit

---
+++ elpa/with-editor-20211014.2146/with-editor-pkg.el
@@ -0,0 +1,12 @@
+(define-package "with-editor" "20211014.2146" "Use the Emacsclient as $EDITOR"
+  '((emacs "24.4"))
+  :commit "63afb66bfb1ba05bc10ba0a58fe2bdcaf1369424" :authors
+  '(("Jonas Bernoulli" . "jonas@bernoul.li"))
+  :maintainer
+  '("Jonas Bernoulli" . "jonas@bernoul.li")
+  :keywords
+  '("tools")
+  :url "https://github.com/magit/with-editor")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
