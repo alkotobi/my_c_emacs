60b11e4b489b2cd93763bea582a5b44ad78d5999
merhab <merhab@gmail.com>
2023-05-14 23:35:46 +0100

    my emacs config

--- config.el
+++ config.el
@@ -1,5 +1,262 @@
-(use-package all-the-icons
-  :ensure t)
+;; NOTE: init.el is now generated from Emacs.org.  Please edit that file
+                ;;       in Emacs and init.el will be generated automatically!
+
+                ;; You will most likely need to adjust this font size for your system!
+                (defvar efs/default-font-size 180)
+                (defvar efs/default-variable-font-size 180)
+
+                ;; Make frame transparency overridable
+                (defvar efs/frame-transparency '(90 . 90))
+
+
+              ;; Initialize package sources
+              (require 'package)
+
+              (setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                                       ("org" . "https://orgmode.org/elpa/")
+                                       ("elpa" . "https://elpa.gnu.org/packages/")))
+
+              (package-initialize)
+              (unless package-archive-contents
+                (package-refresh-contents))
+
+                ;; Initialize use-package on non-Linux platforms
+              (unless (package-installed-p 'use-package)
+                (package-install 'use-package))
+
+              (require 'use-package)
+              (setq use-package-always-ensure t)
+
+
+
+            (setq inhibit-startup-message t)
+
+            (scroll-bar-mode -1)        ; Disable visible scrollbar
+            (tool-bar-mode 1)          ; Disable the toolbar
+            (tooltip-mode -1)           ; Disable tooltips
+            (set-fringe-mode 10)        ; Give some breathing room
+
+            (menu-bar-mode 1)            ; Disable the menu bar
+
+            ;; Set up the visible bell
+            (setq visible-bell t)
+
+            (column-number-mode)
+            (global-display-line-numbers-mode t)
+
+            ;; Set frame transparency
+            (set-frame-parameter (selected-frame) 'alpha efs/frame-transparency)
+            (add-to-list 'default-frame-alist `(alpha . ,efs/frame-transparency))
+            (set-frame-parameter (selected-frame) 'fullscreen 'maximized)
+            (add-to-list 'default-frame-alist '(fullscreen . maximized))
+
+            ;; Disable line numbers for some modes
+            (dolist (mode '(org-mode-hook
+                            term-mode-hook
+                            shell-mode-hook
+                            treemacs-mode-hook
+                            eshell-mode-hook))
+              (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+
+
+          (set-face-attribute 'default nil :font "Fira Code Retina" :height efs/default-font-size)
+
+          ;; Set the fixed pitch face
+          (set-face-attribute 'fixed-pitch nil :font "Fira Code Retina" :height efs/default-font-size)
+
+          ;; Set the variable pitch face
+          (set-face-attribute 'variable-pitch nil :font "Cantarell" :height efs/default-variable-font-size :weight 'regular)
+
+
+
+        ;; Make ESC quit prompts
+        (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+        (use-package general
+          :config
+          (general-create-definer rune/leader-keys
+            :keymaps '(normal insert visual emacs)
+            :prefix "SPC"
+            :global-prefix "C-SPC")
+
+          (rune/leader-keys
+            "t"  '(:ignore t :which-key "toggles")
+            "tt" '(counsel-load-theme :which-key "choose theme")))
+
+        (use-package evil
+          :init
+          (setq evil-want-integration t)
+          (setq evil-want-keybinding nil)
+          (setq evil-want-C-u-scroll t)
+          (setq evil-want-C-i-jump nil)
+          :config
+          (evil-mode 1)
+          (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+          (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
+
+          ;; Use visual line motions even outside of visual-line-mode buffers
+          (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+          (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+
+          (evil-set-initial-state 'messages-buffer-mode 'normal)
+          (evil-set-initial-state 'dashboard-mode 'normal))
+
+        (use-package evil-collection
+          :after evil
+          :config
+          (evil-collection-init))
+
+
+
+      (use-package command-log-mode)
+
+
+    (use-package which-key
+      :init (which-key-mode)
+      :diminish which-key-mode
+      :config
+      (setq which-key-idle-delay 1))
+
+    (use-package ivy
+      :diminish
+      :bind (("C-s" . swiper)
+             :map ivy-minibuffer-map
+             ("TAB" . ivy-alt-done)
+             ("C-l" . ivy-alt-done)
+             ("C-j" . ivy-next-line)
+             ("C-k" . ivy-previous-line)
+             :map ivy-switch-buffer-map
+             ("C-k" . ivy-previous-line)
+             ("C-l" . ivy-done)
+             ("C-d" . ivy-switch-buffer-kill)
+             :map ivy-reverse-i-search-map
+             ("C-k" . ivy-previous-line)
+             ("C-d" . ivy-reverse-i-search-kill))
+      :config
+      (ivy-mode 1))
+
+    (use-package ivy-rich
+      :init
+      (ivy-rich-mode 1))
+
+    (use-package counsel
+      :bind (("C-M-j" . 'counsel-switch-buffer)
+             :map minibuffer-local-map
+             ("C-r" . 'counsel-minibuffer-history))
+      :custom
+      (counsel-linux-app-format-function #'counsel-linux-app-format-function-name-only)
+      :config
+      (counsel-mode 1))
+
+    (use-package helpful
+      :custom
+      (counsel-describe-function-function #'helpful-callable)
+      (counsel-describe-variable-function #'helpful-variable)
+      :bind
+      ([remap describe-function] . counsel-describe-function)
+      ([remap describe-command] . helpful-command)
+      ([remap describe-variable] . counsel-describe-variable)
+      ([remap describe-key] . helpful-key))
+
+    (use-package hydra)
+
+    (defhydra hydra-text-scale (:timeout 4)
+      "scale text"
+      ("j" text-scale-increase "in")
+      ("k" text-scale-decrease "out")
+      ("f" nil "finished" :exit t))
+
+    (rune/leader-keys
+      "ts" '(hydra-text-scale/body :which-key "scale text"))
+
+  
+  (defun efs/lsp-mode-setup ()
+    (setq lsp-headerline-breadcrumb-segments '(path-up-to-project file symbols))
+    (lsp-headerline-breadcrumb-mode))
+
+  (use-package lsp-mode
+    :commands (lsp lsp-deferred)
+    :hook (lsp-mode . efs/lsp-mode-setup)
+    :init
+    (setq lsp-keymap-prefix "C-c l")  ;; Or 'C-l', 's-l'
+    :config
+    (lsp-enable-which-key-integration t))
+
+  
+(use-package lsp-ui
+  :hook (lsp-mode . lsp-ui-mode)
+  :custom
+  (lsp-ui-doc-position 'bottom))
+
+(use-package lsp-treemacs
+  :after lsp)
+
+(use-package lsp-ivy)
+
+(use-package dap-mode
+  ;; Uncomment the config below if you want all UI panes to be hidden by default!
+  ;; :custom
+  ;; (lsp-enable-dap-auto-configure nil)
+  ;; :config
+  ;; (dap-ui-mode 1)
+
+  :config
+  ;; Set up Node debugging
+  (require 'dap-node)
+  (dap-node-setup) ;; Automatically installs Node debug adapter if needed
+
+  ;; Bind `C-c l d` to `dap-hydra` for easy access
+  (general-define-key
+    :keymaps 'lsp-mode-map
+    :prefix lsp-keymap-prefix
+    "d" '(dap-hydra t :wk "debugger")))
+
+ 
+;; Make ESC quit prompts
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+(use-package general
+  :config
+  (general-create-definer rune/leader-keys
+    :keymaps '(normal insert visual emacs)
+    :prefix "SPC"
+    :global-prefix "C-SPC")
+
+  (rune/leader-keys
+    "t"  '(:ignore t :which-key "toggles")
+    "tt" '(counsel-load-theme :which-key "choose theme")))
+
+(use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-C-i-jump nil)
+  :config
+  (evil-mode 1)
+  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
+
+  ;; Use visual line motions even outside of visual-line-mode buffers
+  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+
+  (evil-set-initial-state 'messages-buffer-mode 'normal)
+  (evil-set-initial-state 'dashboard-mode 'normal))
+
+(use-package evil-collection
+  :after evil
+  :config
+  (evil-collection-init))
+ 
+
+              ;;original
+
+
+
+                  (use-package all-the-icons
+                    :ensure t)
 
 (use-package doom-themes
   :ensure t
@@ -58,45 +315,43 @@
 (add-hook 'emacs-lisp-mode-hook 'eldoc-mode)
 (add-hook 'emacs-lisp-mode-hook 'yas-minor-mode)
 (add-hook 'emacs-lisp-mode-hook 'company-mode)
+(setq lsp-completion-provider :capf)
 (defun check-expansion ()
     (save-excursion
       (if (looking-at "\\_>") t
-        (backward-char 1)
-        (if (looking-at "\\.") t
-          (backward-char 1)
-          (if (looking-at "->") t nil)))))
+	(backward-char 1)
+	(if (looking-at "\\.") t
+	  (backward-char 1)
+	  (if (looking-at "->") t nil)))))
   (defun do-yas-expand ()
     (let ((yas/fallback-behavior 'return-nil))
       (yas/expand)))
   (defun tab-indent-or-complete ()
     (interactive)
     (if (minibufferp)
-        (minibuffer-complete)
+	(minibuffer-complete)
       (if (or (not yas/minor-mode)
-              (null (do-yas-expand)))
-          (if (check-expansion)
-              (company-complete-common)
-            (indent-for-tab-command)))))
+	      (null (do-yas-expand)))
+	  (if (check-expansion)
+	      (company-complete-common)
+	    (indent-for-tab-command)))))
 (global-set-key [tab] 'tab-indent-or-complete)
 (add-hook 'after-init-hook 'global-company-mode)
 
-;;(use-package company-lsp
-;;  :ensure t
-;;  :commands company-lsp)
-
 (use-package yasnippet
-  :ensure t
-  :config
-    (yas-reload-all))
-(define-key global-map (kbd "C-c C-y") 'yas-new-snippet)
-(defun yas/org-very-safe-expand ()
-(let ((yas/fallback-behavior 'return-nil)) (yas/expand)))
-(add-hook 'org-mode-hook
-(lambda ()
-(make-variable-buffer-local 'yas/trigger-key)
-(setq yas/trigger-key [tab])
-(add-to-list 'org-tab-first-hook 'yas/org-very-safe-expand)
-(define-key yas/keymap [tab] 'yas/next-field)))
+    :ensure t
+    :config
+      (yas-reload-all))
+  (define-key global-map (kbd "C-c C-y") 'yas-new-snippet)
+  (defun yas/org-very-safe-expand ()
+  (let ((yas/fallback-behavior 'return-nil)) (yas/expand)))
+  (add-hook 'org-mode-hook
+  (lambda ()
+  (make-variable-buffer-local 'yas/trigger-key)
+  (setq yas/trigger-key [tab])
+  (add-to-list 'org-tab-first-hook 'yas/org-very-safe-expand)
+  (define-key yas/keymap [tab] 'yas/next-field)))
+(use-package yasnippet-snippets :ensure t)
 
 (use-package org-bullets
   :ensure t
@@ -279,9 +534,200 @@
    (push '("#+end_src" . "⇤" ) prettify-symbols-alist)
    (prettify-symbols-mode)))
 
-
-(use-package exec-path-from-shell
+(use-package python-mode
   :ensure t
+  :hook (python-mode . lsp-deferred)
+  :custom
+  ;; NOTE: Set these if Python 3 is called "python3" on your system!
+  ;; (python-shell-interpreter "python3")
+  ;; (dap-python-executable "python3")
+  (dap-python-debugger 'debugpy)
   :config
-  (unless (memq system-type '(ms-dos windos-nt cygwin))
-    (exec-path-from-shell-initialize)))
+  (require 'dap-python))
+
+(global-auto-revert-mode t)
+(use-package cmake-ide :ensure t)
+(cmake-ide-setup)
+    (use-package flycheck
+      :ensure t
+      :init (global-flycheck-mode))
+
+
+
+
+
+    (use-package pyvenv
+      :config
+      (pyvenv-mode 1))
+
+
+
+
+
+    (use-package projectile
+      :diminish projectile-mode
+      :config (projectile-mode)
+      :custom ((projectile-completion-system 'ivy))
+      :bind-keymap
+      ("C-c p" . projectile-command-map)
+      :init
+      ;; NOTE: Set this to the folder where you keep your Git repos!
+      (when (file-directory-p "~/Projects/Code")
+        (setq projectile-project-search-path '("~/Projects/Code")))
+      (setq projectile-switch-project-action #'projectile-dired))
+
+    (use-package counsel-projectile
+      :config (counsel-projectile-mode))
+
+    (setq-default dotspacemacs-configuration-layers '(
+      (python :variables python-formatter 'yapf)))
+      (setq-default dotspacemacs-configuration-layers '(
+      (python :variables python-format-on-save t)))
+      (setq-default dotspacemacs-configuration-layers '(
+      (python :variables python-save-before-test nil)))
+      (setq-default dotspacemacs-configuration-layers '(
+        (python :variables python-fill-column 99)))
+        (setq-default dotspacemacs-configuration-layers
+      '((python :variables python-sort-imports-on-save t)))
+
+
+    (use-package magit
+      :custom
+      (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
+
+    ;; NOTE: Make sure to configure a GitHub token before using this package!
+    ;; - https://magit.vc/manual/forge/Token-Creation.html#Token-Creation
+    ;; - https://magit.vc/manual/ghub/Getting-Started.html#Getting-Started
+    ;(use-package forge)
+
+
+
+    (use-package evil-nerd-commenter
+      :bind ("M-/" . evilnc-comment-or-uncomment-lines))
+
+
+
+    (use-package rainbow-delimiters
+      :hook (prog-mode . rainbow-delimiters-mode))
+
+
+
+    (use-package term
+      :config
+      (setq explicit-shell-file-name "bash") ;; Change this to zsh, etc
+      ;;(setq explicit-zsh-args '())         ;; Use 'explicit-<shell>-args for shell-specific args
+
+      ;; Match the default Bash shell prompt.  Update this if you have a custom prompt
+      (setq term-prompt-regexp "^[^#$%>\n]*[#$%>] *"))
+
+
+
+    (use-package vterm
+      :commands vterm
+      :config
+      (setq term-prompt-regexp "^[^#$%>\n]*[#$%>] *")  ;; Set this to match your custom shell prompt
+      ;;(setq vterm-shell "zsh")                       ;; Set this to customize the shell to launch
+      (setq vterm-max-scrollback 10000))
+
+
+    (defun efs/configure-eshell ()
+      ;; Save command history when commands are entered
+      (add-hook 'eshell-pre-command-hook 'eshell-save-some-history)
+
+      ;; Truncate buffer for performance
+      (add-to-list 'eshell-output-filter-functions 'eshell-truncate-buffer)
+
+      ;; Bind some useful keys for evil-mode
+      (evil-define-key '(normal insert visual) eshell-mode-map (kbd "C-r") 'counsel-esh-history)
+      (evil-define-key '(normal insert visual) eshell-mode-map (kbd "<home>") 'eshell-bol)
+      (evil-normalize-keymaps)
+
+      (setq eshell-history-size         10000
+            eshell-buffer-maximum-lines 10000
+            eshell-hist-ignoredups t
+            eshell-scroll-to-bottom-on-input t))
+
+    (use-package eshell-git-prompt)
+
+    (use-package eshell
+      :hook (eshell-first-time-mode . efs/configure-eshell)
+      :config
+
+      (with-eval-after-load 'esh-opt
+        (setq eshell-destroy-buffer-when-process-dies t)
+        (setq eshell-visual-commands '("htop" "zsh" "vim")))
+
+      (eshell-git-prompt-use-theme 'powerline))
+
+
+
+    ;; Make ESC quit prompts
+    (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+    (use-package general
+      :config
+      (general-create-definer rune/leader-keys
+        :keymaps '(normal insert visual emacs)
+        :prefix "SPC"
+        :global-prefix "C-SPC")
+
+      (rune/leader-keys
+        "t"  '(:ignore t :which-key "toggles")
+        "tt" '(counsel-load-theme :which-key "choose theme")))
+
+    (use-package evil
+      :init
+      (setq evil-want-integration t)
+      (setq evil-want-keybinding nil)
+      (setq evil-want-C-u-scroll t)
+      (setq evil-want-C-i-jump nil)
+      :config
+      (evil-mode 1)
+      (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+      (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
+
+      ;; Use visual line motions even outside of visual-line-mode buffers
+      (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+      (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+
+      (evil-set-initial-state 'messages-buffer-mode 'normal)
+      (evil-set-initial-state 'dashboard-mode 'normal))
+
+    (use-package evil-collection
+      :after evil
+      :config
+      (evil-collection-init))
+    (add-hook 'python-mode-hook 'anaconda-mode)
+
+    (use-package nim-mode :ensure t)
+    ;; The `nimsuggest-path' will be set to the value of
+    ;; (executable-find "nimsuggest"), automatically.
+    (setq nimsuggest-path "/usr/local/bin/nimsuggest")
+
+    (defun my--init-nim-mode ()
+      "Local init function for `nim-mode'."
+
+      ;; Just an example, by default these functions are
+      ;; already mapped to "C-c <" and "C-c >".
+      (local-set-key (kbd "M->") 'nim-indent-shift-right)
+      (local-set-key (kbd "M-<") 'nim-indent-shift-left)
+
+      ;; Make files in the nimble folder read only by default.
+      ;; This can prevent to edit them by accident.
+      (when (string-match "/\.nimble/" (or (buffer-file-name) "")) (read-only-mode 1))
+
+      ;; If you want to experiment, you can enable the following modes by
+      ;; uncommenting their line.
+      (nimsuggest-mode 1)
+      ;; Remember: Only enable either `flycheck-mode' or `flymake-mode' at the same time.
+      (flycheck-mode 1)
+      ;; (flymake-mode 1)
+
+      ;; The following modes are disabled for Nim files just for the case
+      ;; that they are enabled globally.
+      ;; Anything that is based on smie can cause problems.
+      (auto-fill-mode 0)
+      (electric-indent-local-mode 0)
+    )
+
+    (add-hook 'nim-mode-hook 'my--init-nim-mode)
--- init.el
+++ init.el
@@ -5,17 +5,29 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(cua-mode t nil (cua-base))
+ '(custom-safe-themes
+   '("7a7b1d475b42c1a0b61f3b1d1225dd249ffa1abb1b7f726aec59ac7ca3bf4dae" "246a9596178bb806c5f41e5b571546bb6e0f4bd41a9da0df5dfbca7ec6e2250c" "745d03d647c4b118f671c49214420639cb3af7152e81f132478ed1c649d4597d" "b0e446b48d03c5053af28908168262c3e5335dcad3317215d9fdeb8bac5bacf9" "e8df30cd7fb42e56a4efc585540a2e63b0c6eeb9f4dc053373e05d774332fc13" "b186688fbec5e00ee8683b9f2588523abdf2db40562839b2c5458fcfb322c8a4" "5784d048e5a985627520beb8a101561b502a191b52fa401139f4dd20acb07607" "97db542a8a1731ef44b60bc97406c1eb7ed4528b0d7296997cbb53969df852d6" "613aedadd3b9e2554f39afe760708fc3285bf594f6447822dd29f947f0775d6c" "f91395598d4cb3e2ae6a2db8527ceb83fed79dbaf007f435de3e91e5bda485fb" default))
  '(frame-resize-pixelwise t)
+ '(helm-minibuffer-history-key "M-p")
  '(inhibit-startup-screen t)
  '(menu-bar-mode nil)
  '(package-archives
    '(("gnu" . "https://elpa.gnu.org/packages/")
      ("melpa" . "https://melpa.org/packages/")))
  '(package-selected-packages
-   '(magit exec-path-from-shell counsel-etags lsp-treemacs treemacs org-bullets company-lsp helm-lsp doom-modeline dashboard quickrun yasnippet company helm use-package async))
+   '(cmake-ide package-install yasnippet-snippets nim-mode dired-hide-dotfiles dired-open all-the-icons-dired dired-single eshell-git-prompt vterm rainbow-delimiters evil-nerd-commenter forge counsel-projectile projectile python-mode anaconda-mode jedi-core pyvenv pyenv-mode counsel-world-clock neotree sr-speedbar cmake-mode magit exec-path-from-shell counsel-etags lsp-treemacs treemacs org-bullets company-lsp helm-lsp doom-modeline dashboard quickrun yasnippet company helm use-package async))
  '(scroll-bar-mode nil)
  '(show-paren-mode t)
- '(tool-bar-mode nil))
+ '(tool-bar-mode nil)
+ '(warning-suppress-log-types
+   '(((package reinitialization))
+     ((package reinitialization))
+     ((package reinitialization))
+     ((package reinitialization))))
+ '(warning-suppress-types
+   '(((package reinitialization))
+     ((package reinitialization))
+     ((package reinitialization)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
